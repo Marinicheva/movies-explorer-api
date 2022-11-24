9002e70d27b69cb80adc4aa31222163017a377d4
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-24 22:55:03 +0300

    feat: add loggers for requests and errors

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 // TODO: Уточнить соответствует ли это требованиям чек-листа
-const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
 const router = require('./routes/index');
 
 const app = express();
 
-mongoose.connect(DB_URL);
+mongoose.connect(MONGO_URL);
 
 // Parsers
 app.use(express.json());
 
+// Логгер запросов
+app.use(requestLogger);
+
 // Routes
 app.use(router);
+
+// Логгер ошибок
+app.use(errorLogger);
 
 // Listening
 app.listen(PORT, () => {
