bd8c41b365cc45f2fe05e56319c040939d56f848
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-25 22:09:14 +0300

    feat: add auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const auth = (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    let payload;
+
+    jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    req.user = payload;
+    next();
+  } catch (err) {
+    // TODO: Централизованная обработка ошибок
+    console.log(err);
+    res.send('Токена нет или он неверный');
+  }
+};
+
+module.exports = { auth };
