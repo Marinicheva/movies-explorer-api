84413c85887fe70047adb15a5392397199a59672
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-26 14:57:31 +0300

    feat: and helmet and requestsLimiter

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 
+const limiter = require('./middlewares/requestsLimiter');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes/index');
 const handleErrors = require('./middlewares/handleErrors');
@@ -17,6 +19,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 mongoose.connect(MONGO_URL);
+
+app.use(limiter);
+app.use(helmet());
 
 // Логгер запросов
 app.use(requestLogger);
